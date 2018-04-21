cbb720f6210eb29f864cf73cff7c31b45f4702b0
Mohamed <mohamed.hany.fcih@gmail.com>
2018-04-21 20:57:13 +0200

    1.2
    list Professor
    admin add Professor

--- GP/Controllers/AdminsController.cs
+++ GP/Controllers/AdminsController.cs
@@ -10,6 +10,7 @@ using Models.Models;
 
 namespace GP.Controllers
 {
+    
     public class AdminsController : Controller
     {
         private GPContext db = new GPContext();
@@ -17,12 +18,60 @@ namespace GP.Controllers
         //GET
         public ActionResult ListProfsssors()
         {
-            return View();
+            return View(db.Professors.ToList());
         }
 
         public ActionResult AddPro()
         {
             return View();
+        }
+
+        // POST: Adminss/AddPro
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddPro([Bind(Include = "id,name,email,password")] Professor professor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Professors.Add(professor);
+                db.SaveChanges();
+                return RedirectToAction("ListProfsssors");
+            }
+
+            return View(professor);
+        }
+
+        // GET: Professors/Delete/{{id}}
+        public ActionResult DeletePro(int? id)
+        {
+            ProfessorsController p = new ProfessorsController();
+
+            return p.Delete(id);
+        }
+
+        public ActionResult EditPro()
+        {
+            return View();
+        }
+
+        // GET: Admins/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Admin/login
+        [HttpPost]
+        public ActionResult login([Bind(Include = "email,password")]  Admin admin)
+        {
+
+            var adminList = db.Admins.Where(m => m.email == admin.email && m.password == admin.password).ToList();
+
+            if (adminList.Count != 0)
+                return RedirectToAction("Index");
+
+            else
+                return RedirectToAction("login");
         }
 
 
